ca52670175bfed89e0c7ec2d45c9d8a6527f0bb8
NicoToff <toffolo01@hotmail.com>
2023-09-27 20:31:33 +0200

    config(windows): avoid eol eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,12 +15,13 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: [".eslintrc.js"],
+  ignorePatterns: [".eslintrc.js", "*.md*"],
   rules: {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/ban-types": "off",
+    "linebreak-style": "off",
   },
 };
